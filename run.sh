63bc295aebd2305496502e7aa7fcd4847fb9df72
Jack Desert <jackdesert@gmail.com>
2017-11-16 15:25:53 -0800

    Stats in bash version

--- run.sh
+++ run.sh
@@ -3,15 +3,27 @@
 
 HOST='google.com'
 
+up_count=0
+down_count=0
 
 while true
 do
   ping -c1 -w4 $HOST > /dev/null 2> /dev/null
   if [ $? == 0 ]; then
-    printf 'up\n'
+    up_count=$((up_count+1))
+    printf 'UP     '
   else
-    printf 'down\n'
+    down_count=$((down_count+1))
+    printf 'DOWN   '
   fi
+
+  printf 'up: '
+  printf $up_count
+
+  printf '   down: '
+  printf $down_count
+
+  printf '\n'
 
   sleep 1
 done
