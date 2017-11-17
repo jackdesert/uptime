5de24aa490982ea6c2fbd0d5068447efd8b115e4
Jack Desert <jackdesert@gmail.com>
2017-11-16 21:44:41 -0800

    Use cURL instead of ping
    because cURL is tcp, whereas ping is icmp
    and icmp is often deprioritized.

--- run.sh
+++ run.sh
@@ -1,7 +1,27 @@
 #! /bin/bash
-# ping HOST and write "up" or "down" to STDOUT
+# Evaluate network uptime
+#
+# WHY cURL?
+# Curl is a simple way to do tcp.
+# We care about tcp because that is how our webserver accesses data.
+# If curl fails, our webserver will fail.
+#
+# In the beginning we used ping (icmp) as our smoke test.
+# But according to https://en.wikipedia.org/wiki/Packet_loss
+# ping is considered low priority traffic, and when they are dropped they are not re-sent.
+#
+# Why Yahoo?
+# Yahoo provides a small payload. (The word "redirect")
+# Yahoo is ubiquitous.
+# Yahoo is assumed to be up all the time.
+#
+# Why Bash?
+# So we don't have to install anything extra to run this.
+# Dependencies change things.
 
-HOST='google.com'
+
+
+HOST='yahoo.com'
 
 up_count=0
 down_count=0
@@ -11,7 +31,7 @@ do
   printf $(date +'%FT%H:%M:%S.%N')
   printf '  '
 
-  ping -c1 -w4 $HOST > /dev/null 2> /dev/null
+  curl --connect-timeout 4 $HOST > /dev/null 2> /dev/null
   if [ $? == 0 ]; then
     up_count=$((up_count+1))
     printf 'UP     '
